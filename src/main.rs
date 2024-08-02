0af638cc8fefd7963c594c109762fdcf59b2cbfc
Rain <rain@sunshowers.io>
2024-08-02 12:04:40 -0700

    test stack exhaustion without large local

--- src/main.rs
+++ src/main.rs
@@ -15,21 +15,27 @@ fn main() {
         UseStackerOpt::No => UseStacker::No,
     };
 
+    let inner = if app.make_large_local {
+        recurse_with_large_local
+    } else {
+        recurse_without_local
+    };
+
     eprintln!(
-        "running with use_stacker = {:?}, new_thread = {}",
-        use_stacker, app.new_thread
+        "running with use_stacker = {:?}, new_thread = {}, make_large_local = {}",
+        use_stacker, app.new_thread, app.make_large_local,
     );
 
     if app.new_thread {
-        std::thread::spawn(move || recurse(app.stack_depth, use_stacker))
+        std::thread::spawn(move || recurse(app.stack_depth, use_stacker, inner))
             .join()
             .unwrap();
     } else {
-        recurse(app.stack_depth, use_stacker);
+        recurse(app.stack_depth, use_stacker, inner);
     }
 }
 
-fn recurse(remaining: usize, use_stacker: UseStacker) {
+fn recurse(remaining: usize, use_stacker: UseStacker, inner: fn(usize, UseStacker)) {
     match use_stacker {
         UseStacker::Always {
             red_zone,
@@ -39,18 +45,24 @@ fn recurse(remaining: usize, use_stacker: UseStacker) {
             red_zone,
             stack_size,
         } => stacker::maybe_grow(red_zone, stack_size, || {
-            recurse_inner(remaining, use_stacker.next())
+            recurse_with_large_local(remaining, use_stacker.next())
         }),
-        UseStacker::No => recurse_inner(remaining, use_stacker.next()),
+        UseStacker::No => inner(remaining, use_stacker.next()),
     }
 }
 
-fn recurse_inner(remaining: usize, use_stacker: UseStacker) {
+fn recurse_without_local(remaining: usize, use_stacker: UseStacker) {
+    if remaining > 0 {
+        recurse(remaining - 1, use_stacker, recurse_without_local);
+    }
+}
+
+fn recurse_with_large_local(remaining: usize, use_stacker: UseStacker) {
     let array = [0u8; 1024];
     // Attempt to ensure that array doesn't get optimized out.
     std::hint::black_box(array);
     if remaining > 0 {
-        recurse(remaining - 1, use_stacker);
+        recurse(remaining - 1, use_stacker, recurse_with_large_local);
     }
 }
 
@@ -58,6 +70,9 @@ fn recurse_inner(remaining: usize, use_stacker: UseStacker) {
 struct StackExhaustTestApp {
     #[clap(long, short = 's', default_value_t, value_enum)]
     use_stacker: UseStackerOpt,
+
+    #[clap(long, short = 'l')]
+    make_large_local: bool,
 
     #[clap(long, short = 't', default_value_t)]
     new_thread: bool,
