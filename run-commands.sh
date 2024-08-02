08b224ccabda74f61e63de8ed5ac367c802d07d2
Rain <rain@sunshowers.io>
2024-08-02 12:20:55 -0700

    set red zone to 4MiB

--- run-commands.sh
+++ run-commands.sh
@@ -1,6 +1,9 @@
 #!/bin/env bash
 
-# Run a bunch of commands.
+# Run a bunch of commands testing various combinations.
+# 
+# Note that the red zone is set to 4MiB to ensure that a segment is always
+# created.
 
 # DO NOT set -e because we want to run all commands even if one of them fails.
 
@@ -14,8 +17,8 @@ function run() {
 run
 run --use-stacker once
 run --use-stacker always
-run --use-stacker once --red-zone $((2 * 1024 * 1024))
+run --use-stacker once --red-zone $((4 * 1024 * 1024))
 run --new-thread
 run --new-thread --use-stacker once
 run --new-thread --use-stacker always
-run --new-thread --use-stacker once --red-zone $((2 * 1024 * 1024))
+run --new-thread --use-stacker once --red-zone $((4 * 1024 * 1024))
