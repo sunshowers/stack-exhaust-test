58742631e72b0c4f04027acce4d3fe7972698275
Rain <rain@sunshowers.io>
2024-08-02 12:24:53 -0700

    bump red zone further

--- run-commands.sh
+++ run-commands.sh
@@ -2,7 +2,7 @@
 
 # Run a bunch of commands testing various combinations.
 # 
-# Note that the red zone is set to 4MiB to ensure that a segment is always
+# Note that the red zone is set to 16MiB to ensure that a segment is always
 # created.
 
 # DO NOT set -e because we want to run all commands even if one of them fails.
@@ -17,8 +17,8 @@ function run() {
 run
 run --use-stacker once
 run --use-stacker always
-run --use-stacker once --red-zone $((4 * 1024 * 1024))
+run --use-stacker once --red-zone $((16 * 1024 * 1024))
 run --new-thread
 run --new-thread --use-stacker once
 run --new-thread --use-stacker always
-run --new-thread --use-stacker once --red-zone $((4 * 1024 * 1024))
+run --new-thread --use-stacker once --red-zone $((16 * 1024 * 1024))
